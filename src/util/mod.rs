c21387180b7ae7f1868b9f4ddd46854edd26ffad
David M <PieKing1215@users.noreply.github.com>
2023-01-06 03:36:36 -0500

    Add find_microcontroller_folder fn

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,1 +1,25 @@
-pub mod fakemap_hack;
+use std::path::PathBuf;
+
+pub(crate) mod fakemap_hack;
+
+/// Finds the path of the user's microcontroller data folder.
+///
+/// It should be at `%appdata%/Stormworks/data/microprocessors/`.
+///
+/// # Errors
+///
+/// Will return an [`Err`] if the path cannot be found.
+pub fn find_microcontroller_folder() -> Result<PathBuf, &'static str> {
+    if let Some(data_dir) = dirs::data_dir() {
+        let mcs = data_dir.join(
+            "Stormworks/data/microprocessors/".replace('/', &std::path::MAIN_SEPARATOR.to_string()),
+        );
+        if mcs.exists() {
+            Ok(mcs)
+        } else {
+            Err("Could not find folder at %appdata%/Stormworks/data/microprocessors/, please specify full path to microprocessors folder.")
+        }
+    } else {
+        Err("Could not find %appdata%, please specify full path to microprocessors folder.")
+    }
+}
--- tests/samples.rs
+++ tests/samples.rs
@@ -3,7 +3,8 @@ use sw_mc::Microcontroller;
 
 #[test]
 fn test_samples_serde_matches() {
-    for f in std::fs::read_dir("samples").unwrap() {
+    let samples = std::fs::read_dir("samples").unwrap();
+    for f in samples {
         let entry = f.unwrap();
         let fname = entry.file_name().into_string().unwrap();
         if fname.ends_with(".xml") {
@@ -18,6 +19,32 @@ fn test_samples_serde_matches() {
                 .unwrap_or_else(|_| panic!("Failed to serialize {fname}"));
 
             assert_str_eq!(src.trim(), out.trim(), "{fname}:\n{mc:#?}");
+        }
+    }
+}
+
+#[test]
+fn test_sw_dir_serde_matches() {
+    let mc_dir = sw_mc::util::find_microcontroller_folder()
+        .ok()
+        .and_then(|p| std::fs::read_dir(p).ok());
+    if let Some(mc_dir) = mc_dir {
+        for f in mc_dir {
+            let entry = f.unwrap();
+            let fname = entry.file_name().into_string().unwrap();
+            if fname.ends_with(".xml") {
+                println!("CHECKING {fname}...");
+                let src = std::fs::read_to_string(entry.path()).unwrap();
+
+                let mc: Microcontroller = quick_xml::de::from_str(&src)
+                    .unwrap_or_else(|_| panic!("Failed to deserialize {fname}"));
+
+                let out = mc
+                    .to_microcontroller_xml()
+                    .unwrap_or_else(|_| panic!("Failed to serialize {fname}"));
+
+                assert_str_eq!(src.trim(), out.trim(), "{fname}:\n{mc:#?}");
+            }
         }
     }
 }
